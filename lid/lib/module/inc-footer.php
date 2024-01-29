f7ac3731c50103ce5f8bf0052756a6944a49aa68
Vladikad <158090928+Vladikad@users.noreply.github.com>
2024-01-29 22:53:38 +0300

    Add files via upload

---
+++ lid/lib/module/inc-footer.php
@@ -0,0 +1,178 @@
+<footer>
+<div class="footer-con-container">
+	<div class="footer-con-overlay darkmode-header">
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class="footer-con-header">
+			<div class="footer-con-outer">
+				<div class="footer-con-inner">
+					<div class="footer-ico-logo darkmode-footer-logo">
+					</div>
+					<table class="footer-table-out">
+					<tr>
+						<td>
+							<div class="footer-table-in">
+								<div class="footer-con-bound">
+									<div class="footer-tx1-bound">
+										</a>
+									</div>
+										</a>
+									</div>
+								</div>
+							</div>
+						</td>
+						<td>
+							<div class="footer-table-in">
+								<div class="footer-con-bound">
+									<div class="footer-tx1-bound">
+										<span>Ссылки</span>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://mcskill.net/" target="_blank">
+										<span>Сайт</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://ru.pixelmon.site/">
+										<span>Вики</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://confluence.mcskill.net/pages/viewpage.action?pageId=18089217" target="_blank">
+										<span>Конфлюенс</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://jira.mcskill.net/" target="_blank">
+										<span>Jira</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://mcskill.net/mcforum/" target="_blank">
+										<span>Форум</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://mcskill.net/mcforum/index.php?/profile/313524-vladikad/" target="_blank">
+										<span>...</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://mcskill.net/mcforum/index.php?/profile/313524-vladikad/" target="_blank">
+										<span>...</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</td>
+						<td>
+							<div class="footer-table-in">
+								<div class="footer-con-bound">
+									<div class="footer-tx1-bound">
+										<span>Соц сети</span>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://www.youtube.com/@McSkillnet./videos" target="_blank">
+										<span>YouTube</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://vk.com/mcskill" target="_blank">
+										<span>ВК</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://t.me/mcskill" target="_blank">
+										<span>Телеграм</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://discord.com/invite/JAckwt3W?utm_source=Discord%20Widget&utm_medium=Connect" target="_blank">
+										<span>Дискорд</span>
+										</a>
+									</div>
+									<div class="footer-tx2-bound">
+										<a href="https://x.com/rpcs3" target="_blank">
+										<span>...</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</td>
+						<td class="footer-con-sources">
+							<div class="footer-table-in">
+								<div class="footer-con-bound">
+									<div class="footer-tx1-bound">
+										</a>
+									</div>
+								</div>
+							</div>
+						</td>
+					</tr>
+					</table>
+				</div>
+			</div>
+		</div>
+		<div class='footer-con-credits'>
+			<div class="footer-con-outer">
+				<div class="footer-con-inner">
+					<div class='footer-con-seprator'>
+					</div>
+					<div class="footer-con-foot">
+						<div class="footer-con-side">
+							<div class="footer-tx1-founder">
+								<div class="footer-ico-container">
+									<div class="footer-ico-developer" style="background: rgb(255 255 255 / 10%) url('/img/icons/footer/dh.png') center center / cover no-repeat;">
+									</div>
+								</div>
+								</a>
+								<div class="footer-ico-container">
+									<div class="footer-ico-developer" style="background: rgb(255 255 255 / 10%) url('/img/icons/footer/hykem.png') center center / cover no-repeat;">
+									</div>
+								</div>
+								</a>
+								<div class="footer-tx1-developer pulsate">
+								</div>
+								</a>
+							</div>
+						</div>
+						<div class="footer-con-side">
+							<div class="footer-tx1-webmaster">
+								<a href="https://mcskill.net/mcforum/index.php?/profile/1230383-iambanana/" target="_blank">
+								<div class="footer-ico-container">
+									<div class="footer-ico-developer" style="background: url('/img/users/IamBANANA.png') center / cover no-repeat;">
+									</div>
+								</div>
+								</a>
+								<a href="https://mcskill.net/mcforum/index.php?/profile/53769-colossus/" target="_blank">
+								<div class="footer-ico-container">
+									<div class="footer-ico-developer" style="background: url('/img/users/colossus.png') center / cover no-repeat;">
+									</div>
+								</div>
+								</a>
+								<a href="https://mcskill.net/mcforum/index.php?/profile/313524-vladikad/" target="_blank">
+								<div class="footer-ico-container">
+									<div class="footer-ico-developer" style="background: url('/img/users/vladikad.png') center / cover no-repeat;">
+									</div>
+								</div>
+								</a>
+								<a href="https://t.me/vladikad0" target="_blank">
+								<div class="footer-tx1-developer pulsate">
+									<span>Website by Vladikad © 2024</span>
+								</div>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</footer>
---
+++ lid/lib/module/sys-meta.php
@@ -0,0 +1,18 @@
+<meta name="author" content="RPCS3">
+<meta name="copyright" content="RPCS3">
+<link rel="icon" type="image/png" href="/img/meta/favicon/McSkill.png" sizes="16x16"/>
+<link rel="icon" type="image/png" href="/img/meta/favicon/McSkill.png" sizes="32x32"/>
+<link rel="icon" type="image/png" href="/img/meta/favicon/McSkill.png" sizes="96x96"/>
+<link rel="icon" type="image/png" href="/img/meta/favicon/McSkill.png" sizes="128x128"/>
+<link rel="icon" type="image/png" href="/img/meta/favicon/McSkill.png" sizes="196x196"/>
+<link rel="apple-touch-icon" href="/img/meta/mobile/McSkill.png" sizes="57x57"/>
+<link rel="apple-touch-icon" href="/img/meta/mobile/McSkill.png" sizes="60x60"/>
+<link rel="apple-touch-icon" href="/img/meta/mobile/McSkill.png" sizes="72x72"/>
+<link rel="apple-touch-icon" href="/img/meta/mobile/McSkill.png" sizes="76x76"/>
+<link rel="apple-touch-icon" href="/img/meta/mobile/McSkill.png" sizes="114x114"/>
+<link rel="apple-touch-icon" href="/img/meta/mobile/McSkill.png" sizes="120x120"/>
+<link rel="apple-touch-icon" href="/img/meta/mobile/McSkill.png" sizes="144x144"/>
+<link rel="apple-touch-icon" href="/img/meta/mobile/McSkill.png" sizes="152x152"/>
+<link rel="manifest" href="/lib/js/manifest.json">
+<meta name="theme-color" content="#686de0">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
