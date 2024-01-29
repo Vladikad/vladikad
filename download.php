573eb861d86f3b01351779bfcbaaf2be684e2007
Vladikad <158090928+Vladikad@users.noreply.github.com>
2024-01-29 22:50:37 +0300

    Add files via upload

---
+++ download.php
@@ -0,0 +1,170 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>Пояснения</title>
+<meta charset="utf-8">
+<meta name="description" content="Our goal is to preserve the legacy of the PlayStation 3 by completely and accurately emulating its vast library in its entirety with the power of open-source community and reverse engineering.">
+<meta name="keywords" content="rpcs3, playstation, playstation 3, ps3, emulator, debugger, windows, linux, macos, freebsd, open source, nekotekina, kd11, about">
+<?php include 'lib/module/sys-meta.php';?>
+<meta property="og:title" content="RPCS3 - About"/>
+<meta property="og:description" content="Our goal is to preserve the legacy of the PlayStation 3 by completely and accurately emulating its vast library in its entirety with the power of open-source community and reverse engineering."/>
+<meta property="og:image" content="https://rpcs3.net/img/meta/mobile/1200.png"/>
+<meta property="og:image:width" content="1200"/>
+<meta property="og:image:height" content="630"/>
+<meta property="og:url" content="https://rpcs3.net"/>
+<meta property="og:locale" content="en_US"/>
+<meta property="og:type" content="website"/>
+<meta property="og:site_name" content="RPCS3"/>
+<meta name="twitter:title" content="RPCS3 - About">
+<meta name="twitter:description" content="Our goal is to preserve the legacy of the PlayStation 3 by completely and accurately emulating its vast library in its entirety with the power of open-source community and reverse engineering.">
+<meta name="twitter:image" content="https://rpcs3.net/img/meta/mobile/1200.png">
+<meta name="twitter:site" content="@rpcs3">
+<meta name="twitter:creator" content="@rpcs3">
+<meta name="twitter:card" content="summary_large_image">
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+</head>
+<body>
+<?php include 'lib/module/sys-php.php';?>
+<div class="page-con-content">
+	<div class="banner-con-container darkmode-header">
+		<div id="object-particles">
+		</div>
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class='banner-con-title fade-up-onstart'>
+			<div class='banner-tx1-title fade-up-onstart pulsate'>
+				<h1>Пояснения</h1>
+			</div>
+			<div class='banner-con-divider'>
+			</div>
+			<div class='banner-tx2-title fade-up-onstart'>
+				<p>
+					 Помогает разобраться в той или иной ситуации.
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="page-con-container">
+		<div class="page-in-container">
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Завуалы/Не завуалы</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+									 <aside>
+💡 Новичку в составе сложно понять когда в чате происходит нарушение правила 3.2, а когда нарушения нет. Речь пойдет о мате и завуалированном мате.
+
+
+</aside>  <br>
+									<br>
+<p><strong>Модерируется</strong></p>
+<p>Все слова с корнем ЁБ (пример: ебало, ебанутся, долбаёб, уебан и тд)<p>
+<p>Все слова с корнями ХУЙ/Е (пример: нахуй, нихуясе, охуенная, хуесос)<p>
+<p>Все слова с корнями БЛЯ (пример: бля, блядь, блеать  и т.д.)<p>
+<p>Все слова с корнями ПИЗД (пример: Пиздец, пиздёжь, спиздил,  пздц, пиздабол и т.д.)<p>
+<p>Акты совокупления, мужской и женский половые органы и их производные (прим.:  потрахаться, дрочить, выражения со словом сперма)<p>
+<p>Завуалированный мат (прим.: мля, збс, пздц и т.д)<p>
+<p>Обсуждение тем сексуального характера<p>
+<p>бля, блеать, мля<p>
+<p>ПНХ, пох, нах<p>
+<p>хули<p>
+<p>фуй<p>
+<p>шароёбится, жидоёб<p>
+<p>хер, херня<p>
+<p>Пропуск/явная замена букв: х”й, е..ать, пиzдец<p>
+<p><strong>Не Модерируется</strong></p>
+<p>хрень<p>
+<p>пипец, ппц<p>
+<p>блеять (про овечек), при явном зав.мате - мут<p>
+<p>накуй (про кузнеца), при явном зав.мате - мут<p>
+<p>фак<p>
+<p>ля, бл<p>
+<p>хз<p>
+<p>Выражения типа "Фак йе"<p>
+<p>Слово "секс" модерироваться не будет.<p>
+<p>"член" - медицинский термин.<p>
+
+
+хрен, хреново
+
+<p><br>ВАЖНО!
+не путать слова хер и хрен. Первое завуал, второе нет.<br><p>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					<div class='splitter-img-wrapper' style="background: url('/img/graphics/about/project.jpg') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-img-container splitter-con-left'>
+					<div class='splitter-img-wrapper' style="background: url('/img/graphics/about/history.jpg') no-repeat center; background-size: cover;">
+					</div>
+				</div>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Наказания</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+
+<aside>
+Если игрок нарушил правила то логично выдать ему соразмерное наказание за нарушение.
+
+Ориентироваться на размер наказания на сайте в правилах не стоит, там они очень условные.
+
+Прежде всего стоит ориентироваться на здравый смысл, насколько сильным нарушением это является
+
+## Система варнов
+
+На некоторых серверах существует система варнов. В основном варн (предупреждение) дается при нарушении правил чата (3.x).
+
+Автоматические наказания по количеству варнов:
+
+- 1 варн - остается в истории
+- 2 варна - кик с сервера, остается в истории
+- 3 варна - мут на 2 часа, остается в истории
+- 4 варна - мут на 4 часа, остается в истории
+
+и так далее добавляется по 2 часа мута за каждый новый варн.
+
+Предупреждения хранятся в истории не более недели.
+
+Также надо учитывать, что мут привязан к плейтайму (если мут на 2 часа, то игроку нужно отыграть 2 реальных часа в игре).
+
+## Как понять на сколько наказывать?
+
+Так как вы уже играли на сервере, то вы видели на сколько выдаются наказания в среднем. Исходя из этого вы также можете делать выводы. Также можете спросить у своих коллег из состава насколько выдавать. Через некоторое время вы привыкните, и будете сами определять, приходит с опытом.
+</aside> <br>
+									<br>
+
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
---
+++ quickstart.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+<title>Гуишка</title>
+<meta charset="utf-8">
+<meta name="description" content="Our goal is to preserve the legacy of the PlayStation 3 by completely and accurately emulating its vast library in its entirety with the power of open-source community and reverse engineering.">
+<meta name="keywords" content="rpcs3, playstation, playstation 3, ps3, emulator, debugger, windows, linux, macos, freebsd, open source, nekotekina, kd11, about">
+<?php include 'lib/module/sys-meta.php';?>
+<meta property="og:title" content="RPCS3 - About"/>
+<meta property="og:description" content="Our goal is to preserve the legacy of the PlayStation 3 by completely and accurately emulating its vast library in its entirety with the power of open-source community and reverse engineering."/>
+<meta property="og:image" content="https://rpcs3.net/img/meta/mobile/1200.png"/>
+<meta property="og:image:width" content="1200"/>
+<meta property="og:image:height" content="630"/>
+<meta property="og:url" content="https://rpcs3.net"/>
+<meta property="og:locale" content="en_US"/>
+<meta property="og:type" content="website"/>
+<meta property="og:site_name" content="RPCS3"/>
+<meta name="twitter:title" content="RPCS3 - About">
+<meta name="twitter:description" content="Our goal is to preserve the legacy of the PlayStation 3 by completely and accurately emulating its vast library in its entirety with the power of open-source community and reverse engineering.">
+<meta name="twitter:image" content="https://rpcs3.net/img/meta/mobile/1200.png">
+<meta name="twitter:site" content="@rpcs3">
+<meta name="twitter:creator" content="@rpcs3">
+<meta name="twitter:card" content="summary_large_image">
+<?php include 'lib/module/sys-css.php';?>
+<?php include 'lib/module/sys-js.php';?>
+</head>
+<body>
+<?php include 'lib/module/sys-php.php';?>
+<div class="page-con-content">
+	<div class="banner-con-container darkmode-header">
+		<div id="object-particles">
+		</div>
+		<div class="wavebar-con-container">
+			<div class="wavebar-con-wrap">
+				<div class="wavebar-svg-object">
+				</div>
+				<div class="wavebar-svg-object">
+				</div>
+			</div>
+		</div>
+		<div class='banner-con-title fade-up-onstart'>
+			<div class='banner-tx1-title fade-up-onstart pulsate'>
+				<h1>Гуишка</h1>
+			</div>
+			<div class='banner-con-divider'>
+			</div>
+			<div class='banner-tx2-title fade-up-onstart'>
+				<p>
+					 Гуишка. Это основной инструмент для младшего мод состава. В ней есть все необходимые команды и т.п
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="page-con-container">
+		<div class="page-in-container">
+			<div class='splitter-con-container'>
+				<div class='splitter-txt-wrapper'>
+					<div class='container-con-block darkmode-block'>
+						<div class='container-con-wrapper'>
+							<div class='container-tx1-block darkmode-txt'>
+								<div class='container-emp-block'>
+								</div>
+								<h2>Гайд по установке.</h2>
+							</div>
+							<div class='container-tx2-block darkmode-txt'>
+								<p>
+
+	<p>- Переходим по <a href="https://disk.yandex.ru/d/llGVBc5YrrbaLA"> ссылке </a>
+    
+	<p>- После того как скачали архив его надо разорхевировать. Это можно сделать обычным переносом папки на рабочий стол.	<p>
+    
+	<p>- После того как мы разорхевировали файл нажимает камбинацию клавиш Win+r. И пишем в окно %appdata%. Нажимаем ок.	<p>
+    
+	<p>- У нас открыватся папка. Ищем McSkill. Открываем ее.	<p>
+    
+	<p>- В ней нам нужна папка updates открываем ее.	<p>
+
+	<p>- Далее нас интересует папка. Pixelmon1122. Заходим в нее.	<p>
+    
+	<p>- Далее мы переносим скачанную папку ранее.	<p>
+    
+	<p>- Нажимаем заменить файлы в папке назначения.	<p>
+    
+	<p>- Все наш конфиг установлен. Перезапускаем игру и радуемся. Ниже скрины из самой гуишки 	<p>									<br>
+									 
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class='splitter-img-container splitter-con-right'>
+					<div class='splitter-img-wrapper' style="background: url('/img/graphics/about/project.jpg') no-repeat center; 
